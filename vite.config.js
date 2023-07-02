2976b4b61a0a2c8dd2badd81e16410128d645c5a
dprice17 <109916870+dprice17@users.noreply.github.com>
2023-07-02 12:14:10 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/*
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,3 +7,98 @@ export default defineConfig({
   base: "/bestbank123",
   plugins: [react()],
 });
+*/
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs/promises";
+
+export default defineConfig({
+  base: "/bestbank123",
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: "./src/index.js",
+      external: ["uuid"],
+    },
+  },
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
+  },
+});
+
+/*import fs from "fs/promises";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(() => ({
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    // loader: "tsx",
+    // include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
+  },
+}));
+
+/*
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import babel from "@rollup/plugin-babel";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({
+      babelHelpers: "bundled",
+      loader: { ".js": "jsx" },
+      presets: [
+        [
+          "@babel/preset-react",
+          {
+            runtime: "automatic",
+          },
+        ],
+      ],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: "src/index.js",
+      external: ["uuid"],
+    },
+  },
+});
+*/
