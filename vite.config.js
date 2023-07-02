6e577d52449c17f60df3a8910a4e2b5a74c6a5ec
dprice17 <109916870+dprice17@users.noreply.github.com>
2023-07-02 13:50:22 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-/*
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/bestbank123",
-  plugins: [react()],
-});
-*/
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs/promises";
@@ -43,62 +32,3 @@ export default defineConfig({
     },
   },
 });
-
-/*import fs from "fs/promises";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig(() => ({
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.jsx?$/,
-    // loader: "tsx",
-    // include: /src\/.*\.[tj]sx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        {
-          name: "load-js-files-as-jsx",
-          setup(build) {
-            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
-              loader: "jsx",
-              contents: await fs.readFile(args.path, "utf8"),
-            }));
-          },
-        },
-      ],
-    },
-  },
-}));
-
-/*
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import babel from "@rollup/plugin-babel";
-
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({
-      babelHelpers: "bundled",
-      loader: { ".js": "jsx" },
-      presets: [
-        [
-          "@babel/preset-react",
-          {
-            runtime: "automatic",
-          },
-        ],
-      ],
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: "src/index.js",
-      external: ["uuid"],
-    },
-  },
-});
-*/
